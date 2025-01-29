a0ad32014a402cba763369aabac84966f651ef12
Dokki <51916722+Dokki3@users.noreply.github.com>
2025-01-30 00:27:00 +0300

    add getting values from textboxes and radioButtons, as well as a trial function for clicking the "Calculate" button.

--- 122 Соди Хомяков/MainWindow.xaml.cs
+++ 122 Соди Хомяков/MainWindow.xaml.cs
@@ -20,6 +20,53 @@ namespace _122_Соди_Хомяков
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        static int function;
+
+        public void f1(object sender, RoutedEventArgs e)
+        {
+            function = 1;
+        }
+
+        public void f2(object sender, RoutedEventArgs e)
+        {
+            function = 2;
+        }
+
+        public void f3(object sender, RoutedEventArgs e)
+        {
+            function = 3;
+        }
+
+        public void ClickButton(object sender, RoutedEventArgs e)
+        {
+            string x = xInput.Text.ToString();
+            string m = mInput.Text.ToString();
+
+            if (x == "" || m == "")
+            {
+                Answer.Text = "Ошибка";
+                return;
+            }
+
+            double xD = Convert.ToDouble(x);
+            double mD = Convert.ToDouble(m);
+
+            switch (function)
+            {
+                case 1:
+                    Answer.Text = (xD + mD).ToString();
+                    break;
+                case 2:
+                    Answer.Text = (xD * mD).ToString();
+                    break;
+                case 3:
+                    Answer.Text = (xD - mD).ToString();
+                    break;
+            }
+
+        }
+
         public MainWindow()
         {
             InitializeComponent();
